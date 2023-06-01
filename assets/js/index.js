ca1d709c295eacf65414ea3837c27c7cc568a6dd
VitorBarbosa1 <vitor.barbosasilva@mail.itsligo.ie>
2023-06-01 01:59:51 +0100

    parallax scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,14 +5,18 @@ const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
 // Set the canvas width and height to match the window's inner width and height
-canvas.width = 1024;
+canvas.width  = 1024;
 canvas.height = 576;
-
 
 
 let platformImage = new Image();
 platformImage.src = "/Mario-Browser-Game-Tutorial-FullGame-Example/assets/images/platform.png";
 
+let hillsImage = new Image();
+hillsImage.src = "/Mario-Browser-Game-Tutorial-FullGame-Example/assets/images/hills.png";
+
+let backgroundImage = new Image();
+backgroundImage.src = "/Mario-Browser-Game-Tutorial-FullGame-Example/assets/images/background.png";
 
 
 // Define the gravity constant for the game
@@ -26,18 +30,18 @@ class Player
 		// Initialize the player's position, velocity, dimensions, and color
 		this.position = {x: 100, y: 100};
 		this.velocity = {x: 0, y: 0};
-		this.width = 30;
-		this.height = 30;
-		this.color = 'red';
+		this.width    = 30;
+		this.height   = 30;
+		this.color    = 'red';
 	}
-
+	
 	// Define the draw method to draw the player on the canvas
 	draw()
 	{
 		c.fillStyle = this.color;
 		c.fillRect(this.position.x, this.position.y, this.width, this.height);
 	}
-
+	
 	// Define the update method to update the player's position and velocity
 	update()
 	{
@@ -46,10 +50,10 @@ class Player
 		this.position.y += this.velocity.y;
 		this.position.x += this.velocity.x;
 	}
-
+	
 	checkBounds()
 	{
-
+		
 		// If the player is not at the bottom of the canvas, apply gravity
 		if (this.position.y + this.height + this.velocity.y <= canvas.height)
 			{
@@ -60,36 +64,62 @@ class Player
 			{
 				this.velocity.y = 0;
 			}
-
+		
 	}
 }
 
 class Platform
 {
-	constructor({x,y,image})
+	constructor({x, y, image})
 	{
 		this.position = {x: x, y: y};
-		this.image = image;
-		this.width = image.width;
-		this.height = image.height;
+		this.image    = image;
+		this.width    = image.width;
+		this.height   = image.height;
 		
 		
 	}
-
+	
 	draw()
 	{
 		c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
 	}
-
+	
 }
 
+class GenericObject
+{
+	constructor({x, y, image})
+	{
+		this.position = {x: x, y: y};
+		this.image    = image;
+		this.width    = image.width;
+		this.height   = image.height;
+		
+		
+	}
+	
+	draw()
+	{
+		c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+	}
+	
+}
+
+
 // Create a new player object
-const player = new Player();
+const player    = new Player();
 //Create a new platform array object
 const platforms = [
 	new Platform({x: -1, y: 470, image: platformImage}),
-	new Platform({x: platformImage.width-2, y: 470, image: platformImage}),
-	new Platform({x: platformImage.width-2 *2, y: 470, image: platformImage})
+	new Platform({x: platformImage.width - 2, y: 470, image: platformImage}),
+	new Platform({x: platformImage.width - 2 * 2, y: 470, image: platformImage}),
+];
+
+const genericObjects = [
+	new GenericObject({x: -1, y: -1, image: backgroundImage}),
+	new GenericObject({x: 5, y: -1, image: hillsImage}),
+	new GenericObject({x: 20, y: -1, image: hillsImage}),
 ];
 
 const keys = {
@@ -111,6 +141,11 @@ function animate()
 	// Clear the canvas
 	c.fillStyle = 'white';
 	c.fillRect(0, 0, canvas.width, canvas.height);
+	
+	genericObjects.forEach(genericObject =>
+	                       {
+		                       genericObject.draw();
+	                       });
 	
 	platforms.forEach(platform =>
 	                  {
@@ -143,27 +178,37 @@ function animate()
 	else
 		{
 			player.velocity.x = 0;
-			if(keys.right.pressed){
-				scrollOffset += 5;
-				platforms.forEach(platform =>
-				                  {
-					                  platform.position.x -= 5;
-				                  });
-			}
-			else if(keys.left.pressed){
-				scrollOffset -= 5;
-				platforms.forEach(platform =>
-				                  {
-					                  platform.position.x += 5;
-				                  });
-				
-			}
+			if (keys.right.pressed)
+				{
+					scrollOffset += 5;
+					platforms.forEach(platform =>
+					                  {
+						                  platform.position.x -= 5;
+					                  });
+					genericObjects.forEach(genericObject =>
+					                       {
+						                       genericObject.position.x -= 3;
+					                       });
+				}
+			else if (keys.left.pressed)
+				{
+					scrollOffset -= 5;
+					platforms.forEach(platform =>
+					                  {
+						                  platform.position.x += 5;
+					                  });
+					genericObjects.forEach(genericObject =>
+					                       {
+						                       genericObject.position.x += 3;
+					                       });
+				}
 		}
 	
-	if(scrollOffset > 2000){
-		console.log("You win!");
-	}
-
+	if (scrollOffset > 2000)
+		{
+			console.log("You win!");
+		}
+	
 }
 
 // Start the animation loop
